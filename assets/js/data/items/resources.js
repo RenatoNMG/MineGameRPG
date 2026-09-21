411648b945d546a4094661637bf77b521961d028
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-21 20:01:39 -0300

    V1.17: adicionar ovo como recurso coletável

--- assets/js/data/items/resources.js
+++ assets/js/data/items/resources.js
@@ -1,4 +1,5 @@
 export const RESOURCE_ITEMS={
   wood:{id:"wood",name:"Madeira",icon:"🪵",type:"resource",maxStack:99},
-  stone:{id:"stone",name:"Pedra",icon:"🪨",type:"resource",maxStack:99}
+  stone:{id:"stone",name:"Pedra",icon:"🪨",type:"resource",maxStack:99},
+  egg:{id:"egg",name:"Ovo",icon:"🥚",type:"resource",maxStack:99}
 };
