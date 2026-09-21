07e636128739a1b2696e4c9740f058fce2f93017
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-21 19:58:07 -0300

    V1.16: separar itens de ferramentas

---
+++ assets/js/data/items/tools.js
@@ -0,0 +1,5 @@
+export const TOOL_ITEMS={
+  woodenAxe:{id:"woodenAxe",name:"Machado de madeira",icon:"🪓",type:"tool",maxStack:1,tool:"axe"},
+  pickaxe:{id:"pickaxe",name:"Picareta de madeira",icon:"⛏️",type:"tool",maxStack:1,tool:"pickaxe"},
+  axe:{id:"axe",name:"Machado",icon:"🪓",type:"tool",maxStack:1,tool:"axe"}
+};
