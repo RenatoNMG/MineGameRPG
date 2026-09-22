52bd69f473a7978cbd521acc992b3e3dc41878ef
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-22 19:34:15 -0300

    V1.45 padronizar dados das ferramentas

--- assets/js/data/items/tools.js
+++ assets/js/data/items/tools.js
@@ -1,5 +1,5 @@
 export const TOOL_ITEMS={
-  woodenAxe:{id:"woodenAxe",name:"Machado de madeira",icon:"🪓",type:"tool",maxStack:1,tool:"axe"},
-  pickaxe:{id:"pickaxe",name:"Picareta de madeira",icon:"⛏️",type:"tool",maxStack:1,tool:"pickaxe"},
-  axe:{id:"axe",name:"Machado",icon:"🪓",type:"tool",maxStack:1,tool:"axe"}
+  woodenAxe:{id:"woodenAxe",name:"Machado de madeira",icon:"🪓",category:"tool",maxStack:1,tool:"axe",visual:"axe"},
+  pickaxe:{id:"pickaxe",name:"Picareta de madeira",icon:"⛏️",category:"tool",maxStack:1,tool:"pickaxe",visual:"pickaxe"},
+  axe:{id:"axe",name:"Machado",icon:"🪓",category:"tool",maxStack:1,tool:"axe",visual:"axe"}
 };
