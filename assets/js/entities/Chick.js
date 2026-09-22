17bd50cb499e3cd45748e1f603835871923ecaf3
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-22 19:44:50 -0300

    V1.46: corrige travamento dos pintinhos e aumenta velocidade

--- assets/js/entities/Chick.js
+++ assets/js/entities/Chick.js
@@ -1,5 +1,5 @@
 export class Chick{
-  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.speed=24;this.timer=.5+Math.random()*2;}
+  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.speed=32;this.timer=.5+Math.random()*2;}
   update(dt,world){
     this.timer-=dt;
     const d=Math.hypot(this.x-world.player.x,this.y-world.player.y);
@@ -13,7 +13,7 @@ export class Chick{
       if(this.timer<=0){this.dir=Math.random()<.5?-1:1;this.timer=1+Math.random()*2.5;}
       const nx=this.x+this.dir*this.speed*.35*dt;
       const ny=this.y+Math.sin((this.x+this.y)*.03)*this.speed*.08*dt;
-      if(!world.objectBlocks(nx,ny,7,this)){this.x=nx;this.y=ny;}
+      if(!world.objectBlocks(nx,ny,7,null,this)){this.x=nx;this.y=ny;}
       else{this.dir*=-1;this.timer=.2+Math.random()*.5;}
     }
     this.x=Math.max(25,Math.min(world.width-25,this.x));
