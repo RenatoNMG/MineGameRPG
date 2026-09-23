c041de113c889482b5e7ea5894dc48d381334855
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-22 21:40:55 -0300

    Adiciona morte das galinhas e drop de carne

--- assets/js/systems/SpawnSystem.js
+++ assets/js/systems/SpawnSystem.js
@@ -1,11 +1,18 @@
 import {Chick} from "../entities/Chick.js";
 import {Chicken} from "../entities/Chicken.js";
+import {createItem} from "../data/items/index.js";
 
 export class SpawnSystem{
   constructor(world){this.world=world;this.eggHatchTime=90;}
   update(dt){
     const w=this.world;
-    w.chickens.forEach(ch=>ch.update(dt,w));
+    for(let i=w.chickens.length-1;i>=0;i--){
+      const chicken=w.chickens[i];
+      if(chicken.update(dt,w)){
+        w.droppedItems.push({...createItem("chickenMeat",1),x:chicken.x,y:chicken.y});
+        w.chickens.splice(i,1);
+      }
+    }
     for(let i=w.chicks.length-1;i>=0;i--){
       const chick=w.chicks[i];
       chick.update(dt,w);
