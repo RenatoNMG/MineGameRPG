d015178e6543ec2b81a4c74851e1d8a0a705e492
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-22 22:50:39 -0300

    V1.67: carne de galinha apodrece depois de tempo no chão

--- assets/js/systems/SpawnSystem.js
+++ assets/js/systems/SpawnSystem.js
@@ -3,13 +3,13 @@ import {Chicken} from "../entities/Chicken.js";
 import {createItem} from "../data/items/index.js";
 
 export class SpawnSystem{
-  constructor(world){this.world=world;this.eggHatchTime=90;}
+  constructor(world){this.world=world;this.eggHatchTime=90;this.meatRotTime=120;}
   update(dt){
     const w=this.world;
     for(let i=w.chickens.length-1;i>=0;i--){
       const chicken=w.chickens[i];
       if(chicken.update(dt,w)){
-        w.droppedItems.push({...createItem("chickenMeat",1),x:chicken.x,y:chicken.y});
+        w.droppedItems.push({...createItem("chickenMeat",1),x:chicken.x,y:chicken.y,dropAge:0});
         w.chickens.splice(i,1);
       }
     }
@@ -27,6 +27,18 @@ export class SpawnSystem{
       if(egg.age>=this.eggHatchTime){
         w.chicks.push(new Chick(egg.x,egg.y,Math.random()<.5?-1:1));
         w.eggs.splice(i,1);
+      }
+    }
+    for(const item of w.droppedItems){
+      if(item.id!=="chickenMeat")continue;
+      item.dropAge=(item.dropAge||0)+dt;
+      if(item.dropAge>=this.meatRotTime){
+        item.id="rottenMeat";
+        item.name="Carne Podre";
+        item.icon="🤢";
+        item.visual="rottenMeat";
+        item.effects={hunger:5};
+        item.dropAge=0;
       }
     }
   }
