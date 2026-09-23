202dc5ebc3ff942cc9897af7ee410a2a690412e2
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-23 18:56:43 -0300

    V2.20: adiciona estado carregado ao galo

--- assets/js/entities/Rooster.js
+++ assets/js/entities/Rooster.js
@@ -1,3 +1,3 @@
 export class Rooster{
-  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.speed=26;this.timer=.5+Math.random()*2;this.mateTimer=2+Math.random()*4;this.matingTimer=0;this.matingCooldown=0;}
+  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.speed=26;this.timer=.5+Math.random()*2;this.mateTimer=2+Math.random()*4;this.matingTimer=0;this.matingCooldown=0;this.carried=false;}
 }
