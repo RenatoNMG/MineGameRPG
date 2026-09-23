1b649c3f80965735aa172204fa3eb85f34ec9ad0
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-23 10:38:30 -0300

    V1.75: integra AnimalSystem ao mundo

--- assets/js/core/World.js
+++ assets/js/core/World.js
@@ -1,19 +1,16 @@
 import {Config} from "./Config.js";
-import {Enemy} from "../entities/Enemy.js";
 import {WorldGenerator} from "../systems/WorldGenerator.js";
 import {CollisionSystem} from "../systems/CollisionSystem.js";
 import {WorldQuery} from "../systems/WorldQuery.js";
 import {SpawnSystem} from "../systems/SpawnSystem.js";
+import {AnimalSystem} from "../systems/AnimalSystem.js";
 
 export class World{
   constructor(player){
     this.width=Config.WORLD.width;this.height=Config.WORLD.height;this.player=player;
     this.trees=[];this.stones=[];this.looseWood=[];this.chickens=[];this.chicks=[];this.eggs=[];this.waterPuddles=[];this.droppedItems=[];this.enemies=[];
-    const generated=WorldGenerator.generate({player,width:this.width,height:this.height});
-    Object.assign(this,generated);
-    this.collision=new CollisionSystem(this);
-    this.query=new WorldQuery(this);
-    this.spawnSystem=new SpawnSystem(this);
+    Object.assign(this,WorldGenerator.generate({player,width:this.width,height:this.height}));
+    this.collision=new CollisionSystem(this);this.query=new WorldQuery(this);this.animalSystem=new AnimalSystem(this);this.spawnSystem=new SpawnSystem(this);
   }
   objectBlocks(x,y,r=0,ignoreChicken=null,ignoreChick=null){return this.collision.objectBlocks(x,y,r,ignoreChicken,ignoreChick);}
   waterBlocks(x,y,r=0){return this.collision.waterBlocks(x,y,r);}
@@ -25,5 +22,5 @@ export class World{
   findEgg(range=50){return this.query.findEgg(range);}
   findStone(range=50){return this.query.findStone(range);}
   spawnEnemy(){}
-  update(dt){this.trees.forEach(t=>t.update(dt));this.spawnSystem.update(dt);}
+  update(dt){this.trees.forEach(t=>t.update(dt));this.animalSystem.update(dt);this.spawnSystem.update(dt);}
 }
