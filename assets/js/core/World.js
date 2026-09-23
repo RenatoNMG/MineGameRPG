4c892ca900ca63659d943724a3634c8d31257038
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-23 10:42:24 -0300

    V1.76: adiciona TransformationSystem ao mundo

--- assets/js/core/World.js
+++ assets/js/core/World.js
@@ -4,13 +4,14 @@ import {CollisionSystem} from "../systems/CollisionSystem.js";
 import {WorldQuery} from "../systems/WorldQuery.js";
 import {SpawnSystem} from "../systems/SpawnSystem.js";
 import {AnimalSystem} from "../systems/AnimalSystem.js";
+import {TransformationSystem} from "../systems/TransformationSystem.js";
 
 export class World{
   constructor(player){
     this.width=Config.WORLD.width;this.height=Config.WORLD.height;this.player=player;
     this.trees=[];this.stones=[];this.looseWood=[];this.chickens=[];this.chicks=[];this.eggs=[];this.waterPuddles=[];this.droppedItems=[];this.enemies=[];
     Object.assign(this,WorldGenerator.generate({player,width:this.width,height:this.height}));
-    this.collision=new CollisionSystem(this);this.query=new WorldQuery(this);this.animalSystem=new AnimalSystem(this);this.spawnSystem=new SpawnSystem(this);
+    this.collision=new CollisionSystem(this);this.query=new WorldQuery(this);this.animalSystem=new AnimalSystem(this);this.spawnSystem=new SpawnSystem(this);this.transformationSystem=new TransformationSystem(this);
   }
   objectBlocks(x,y,r=0,ignoreChicken=null,ignoreChick=null){return this.collision.objectBlocks(x,y,r,ignoreChicken,ignoreChick);}
   waterBlocks(x,y,r=0){return this.collision.waterBlocks(x,y,r);}
@@ -22,5 +23,5 @@ export class World{
   findEgg(range=50){return this.query.findEgg(range);}
   findStone(range=50){return this.query.findStone(range);}
   spawnEnemy(){}
-  update(dt){this.trees.forEach(t=>t.update(dt));this.animalSystem.update(dt);this.spawnSystem.update(dt);}
+  update(dt){this.trees.forEach(t=>t.update(dt));this.animalSystem.update(dt);this.transformationSystem.update(dt);this.spawnSystem.update(dt);}
 }
