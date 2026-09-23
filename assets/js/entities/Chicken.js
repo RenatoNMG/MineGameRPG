4c949031165890d89dd113acbb308e62fed4b56b
Renato <99817306+RenatoNMG@users.noreply.github.com>
2026-09-22 23:13:01 -0300

    V1.70: permite estado de galinha carregada

--- assets/js/entities/Chicken.js
+++ assets/js/entities/Chicken.js
@@ -1,6 +1,7 @@
 export class Chicken{
-  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.speed=24;this.timer=.5+Math.random()*2;this.eggTimer=20+Math.random()*40;this.age=0;this.lifespan=180;}
+  constructor(x,y,dir=1){this.x=x;this.y=y;this.dir=dir;this.carried=false;this.speed=24;this.timer=.5+Math.random()*2;this.eggTimer=20+Math.random()*40;this.age=0;this.lifespan=180;}
   update(dt,world){
+    if(this.carried)return false;
     this.age+=dt;
     this.timer-=dt;
     this.eggTimer-=dt;
